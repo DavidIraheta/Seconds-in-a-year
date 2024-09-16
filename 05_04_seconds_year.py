6fe9bbd0514545ecd44e3f914e0d3a34491eacb9
DavidIraheta <david.iraheta.89@gmail.com>
2024-09-16 10:15:07 -0600

    Update 05_04_seconds_year.py

--- 05_04_seconds_year.py
+++ 05_04_seconds_year.py
@@ -2,5 +2,5 @@
 # Then write the code in this script file below this line
 # Number of days in a year = 365
 # Seconds in a day = minutes 60 * 60 seconds a minute = 3600
-# 360 seonds * 24 hours in a day 
+# 3600 seconds * 24 hours in a day 
 print(3600*24)
